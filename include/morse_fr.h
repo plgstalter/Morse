680a06ff37083b4691115a50f8dc8585f2973701
plgstalter <pierrelouisgstalter67@gmail.com>
2021-01-20 23:10:39 +0100

    ok

--- include/morse_fr.h
+++ include/morse_fr.h
@@ -1,9 +1,8 @@
 #pragma once
 std::vector<const char *> morse_alphabet();
-std::vector<const char *> alphabet();
-bool separation(std::vector<const char*> vecteur, int rang);
-bool separation_word(std::vector<const char*> vecteur, int rang);
-std::vector<std::vector<const char *> > separate_words(std::vector<const char*> morse);
-std::vector<std::vector<const char *> >  separate_morse(std::vector<const char *> morse);
-std::vector<const char *> morse_to_fr(std::vector<const char *> morse);
-std::vector<const char *> fr_to_morse(std::vector<const char *> fr);
+
+bool separation(std::string vecteur, int rang);
+bool separation_word(std::string vecteur, int rang);
+std::vector<std::string> separate_words(std::string morse);
+std::string decode(std::string morse);
+std::vector<const char *> fr_to_morse(std::string fr);
--- main.cpp
+++ main.cpp
@@ -9,42 +9,19 @@
 
 int main(int argc, char const *argv[])
 {
-    if (argc < 2) {
-        std::cerr << "Veuillez renseigner ce que vous voulez faire : 'code' pour passer de français à morse, 'decode' pour le contraire" << std::endl;
-    }
-    else if (strcmp(argv[1],"code") == 0)
-    {
-        std::cout << "Entrez votre texte à coder" << std::endl;
-        // on récupère le texte à coder
-        std::vector<const char *> a_coder;
-        char index[100];
-        std::cin.get(index, 100);
-        for (int i=0; i<98; i++) {
-            if (index[i]==index[i+1] && index[i+1]==index[i+2]) {
-                break;
-            }
-            a_coder.push_back(&index[i]);
-        }
-        //on encode en morse
-        std::vector<const char *> morse_code = fr_to_morse(a_coder);
-        std::cout << "c'est bon ça a marché" << std::endl;
-        for (int i=0; i<morse_code.size(); i++) {
-            std::cout << morse_code[i];
+    std::cout << "Entrez votre texte" << std::endl;
+    std::string entree;
+    std::getline(std::cin, entree);
+    if (strcmp(argv[1], "code")==0) {
+        std::vector<const char *> res = fr_to_morse(entree);
+        for (int i=0; i<res.size(); i++) {
+            std::cout << res[i];
         }
         std::cout << " " << std::endl;
-        // on génère le fichier audio
     }
-    else if (strcmp(argv[1],"decode") == 0)
-    {
-        std::cout << "Entrez votre fichier audio à décoder" << std::endl;
-        // on récupère le fichier .wav
-        // on le transforme en morse
-        // on décode
-        // on génère le fichier texte
-    }
-    else
-    {
-        std::cerr << "Veuillez renseigner ce que vous voulez faire : 'code' pour passer de français à morse, 'decode' pour le contraire" << std::endl;
+    else if (strcmp(argv[1], "decode")==0) {
+        std::string decodee = decode(entree);
+        std::cout << decodee << std::endl;
     }
     return 0;
 }
