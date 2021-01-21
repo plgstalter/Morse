1db465e1464bb1e9404e566b2e65dddf01cdc433
plgstalter <pierrelouisgstalter67@gmail.com>
2021-01-21 10:07:09 +0100

    début écriture wav

--- src/audio_txt.cpp
+++ src/audio_txt.cpp
@@ -1,16 +1,120 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <cmath>
+#include <fstream>
+#include <string>
+using namespace std;
 
-std::vector<const char *> audio_to_txt() {
-    std::vector<const char *> resultat;  
-    return resultat;
+namespace little_endian_io
+{
+  template <typename Word>
+  std::ostream& write_word( std::ostream& outs, Word value, unsigned size = sizeof( Word ) )
+  {
+    for (; size; --size, value >>= 8)
+      outs.put( static_cast <char> (value & 0xFF) );
+    return outs;
+  }
+}
+using namespace little_endian_io;
+
+void write_beep(double m) {
+    const double two_pi = 6.283185307179586476925286766559;
+    const double max_amplitude = 32760;  // "volume"
+
+    double hz        = 44100;    // samples per second
+    double frequency = 261.626;  // middle C
+    double seconds   = m*0.5;      // time
+
+    int N = hz * seconds;  // total number of samples
+    for (int n = 0; n < N; n++)
+    {
+        double amplitude = (double)n / N * max_amplitude;
+        double value     = sin( (two_pi * n * frequency) / hz );
+        write_word( f, (int)(                 amplitude  * value), 2 );
+        write_word( f, (int)((max_amplitude - amplitude) * value), 2 );
+    }
 }
 
-void txt_to_audio(std::vector<const char *>txt) {
-    //a faire
+void write_silence(double m) {
+    const double two_pi = 6.283185307179586476925286766559;
+    const double max_amplitude = 32760;  // "volume"
+
+    double hz        = 44100;    // samples per second
+    double seconds   = m*0.5;      // time
+
+    int N = hz * seconds;  // total number of samples
+    for (int n = 0; n < N; n++)
+    {
+        double amplitude = (double)n / N * max_amplitude;
+        double value     = 0.0;
+        write_word( f, (int)(                 amplitude  * value), 2 );
+        write_word( f, (int)((max_amplitude - amplitude) * value), 2 );
+    }
 }
 
-void write_letter() {
-    //a faire
+void end_audio(ofstream f, size_t data_chunk_pos) {
+    // (We'll need the final file size to fix the chunk sizes above)
+    size_t file_length = f.tellp();
+
+    // Fix the data chunk header to contain the data size
+    f.seekp( data_chunk_pos + 4 );
+    write_word( f, file_length - data_chunk_pos + 8 );
+
+    // Fix the file header to contain the proper RIFF chunk size, which is (file size - 8) bytes
+    f.seekp( 0 + 4 );
+    write_word( f, file_length - 8, 4 ); 
+}
+
+int main(int argc, char const *argv[])
+{
+    // get the name
+    const char * path;
+    if (argc > 1) {
+        path = argv[1];
+    }
+    else {
+        std::cout << "file path ?" << std::endl;
+        std::string entree;
+        std::getline(std::cin, entree);
+        path = entree.c_str();
+    }
+
+
+    // file init
+    ofstream f(path, ios::binary );
+
+    // Write the file headers
+    f << "RIFF----WAVEfmt ";     // (chunk size to be filled in later)
+    write_word( f,     16, 4 );  // no extension data
+    write_word( f,      1, 2 );  // PCM - integer samples
+    write_word( f,      2, 2 );  // two channels (stereo file)
+    write_word( f,  44100, 4 );  // samples per second (Hz)
+    write_word( f, 176400, 4 );  // (Sample Rate * BitsPerSample * Channels) / 8
+    write_word( f,      4, 2 );  // data block size (size of two integer samples, one for each channel, in bytes)
+    write_word( f,     16, 2 );  // number of bits per sample (use a multiple of 8)
+
+    // Write the data chunk header
+    size_t data_chunk_pos = f.tellp();
+    f << "data----";  // (chunk size to be filled in later)
+    
+
+
+    //write sound
+    
+    
+
+
+
+    // file closing
+    // (We'll need the final file size to fix the chunk sizes above)
+    size_t file_length = f.tellp();
+
+    // Fix the data chunk header to contain the data size
+    f.seekp( data_chunk_pos + 4 );
+    write_word( f, file_length - data_chunk_pos + 8 );
+
+    // Fix the file header to contain the proper RIFF chunk size, which is (file size - 8) bytes
+    f.seekp( 0 + 4 );
+    write_word( f, file_length - 8, 4 ); 
+
+    return 0;
 }
